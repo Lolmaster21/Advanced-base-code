397310f6f0224abcc74ede5334378f1cefb00aec
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-01-09 08:17:42 -0700

    Update Sequential Search.cpp

--- Sequential Search.cpp
+++ Sequential Search.cpp
@@ -1,21 +1,26 @@
+//Sequential search code
+//given user input, this code walks throught the slots of an array from beginning to end
+//and prints out result
+
+
 #include<iostream>
 using namespace std;
 
 int main() {
-	int location;
-	bool found = false;
-	location = 0;
+	int location; //to keep track of which slot in the array we're looking at
+	bool found = false; //have we fount it?
+	location = 0; //start of the first slot
 	int IDs[] = { 234,567,321,567,234,789 };
 
 	int searchItem;
 	cout << "Enter ID you are searching for: " << endl;
 	cin >> searchItem;
 
-	while (location < sizeof(IDs) && !found) {
-		if (IDs[location] == searchItem)
+	while (location < sizeof(IDs) && !found) { //keeping going as long as we haven't reached the end AND we haven't found it
+		if (IDs[location] == searchItem)//check if what were looking for is in that slot
 			found = true;
 		else
-			location++;
+			location++; //if we haven't found it, move to the next slot before looping 
 
 	}
 		 
